8709da97d359ad79d6b133a54cc9bcab5b3804af
blakeM42-school <141346707+blakeM42-school@users.noreply.github.com>
2024-03-13 14:05:38 -0600

    Update neural net (draft).py

--- neural net (draft).py
+++ neural net (draft).py
@@ -6,7 +6,11 @@ def get_unique_word_lengths(passage):
     unique_word_lengths = [len(word) for word in unique_words]
     return unique_words, len(unique_words), unique_word_lengths
 
-passage = "Beneath the twilight sky, a radiant sun dipped, casting hues of orange and pink. A gentle breeze whispered through rustling leaves as day embraced the serenity, promising a night filled with dreams under the celestial canopy, adorned with stars that sparkled in the tranquil cosmic expanse."
+#passage = "Beneath the twilight sky, a radiant sun dipped, casting hues of orange and pink. A gentle breeze whispered through rustling leaves as day embraced the serenity, promising a night filled with dreams under the celestial canopy, adorned with stars that sparkled in the tranquil cosmic expanse."
+#passage = "Amidst emerald meadows, butterflies pirouette, and wildflowers sway in a gentle cold breeze. Sunlight weaves through the brown leaves, creating a beautiful mosaic on the earth. A babbling brook murmurs tales, harmonizing with the birds' incessant melodies. Nature's devine canvas paints serenity, inviting tranquility."
+#passage = "Upon a mountaintop, where air embraces, a lone eagle soars, wings spread in freedom. Valleys below cradle rivers that meander like liquid silver. Pine trees stand sentinel, their whispers carried by the wind. Nature's grandeur, an ode to the sublime, unfolds majestically."
+passage = 
+
 check_unique_words, check_unique_word_count, input = get_unique_word_lengths(passage)
 
 print(check_unique_words)
@@ -19,6 +23,7 @@ biases = [0.7, -0.5, 0.2, 0.8, 0.5, 0.1, 0.9, 0.3]
 output = np.dot(weights, input) + biases
 print(output)
 
+input_passages = [passage, ]
 
 
 
