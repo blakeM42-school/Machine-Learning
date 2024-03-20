204313c1878d5cfb1278d38d5685c6e450be758c
blakeM42-school <141346707+blakeM42-school@users.noreply.github.com>
2024-03-20 10:42:17 -0600

    Add files via upload

---
+++ test.py
@@ -0,0 +1,51 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import nnfs
+
+nnfs.init()
+'''
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+
+layer1 = Layer_Dense(40, 10)
+layer1.forward()
+print(layer1.output)
+layer2 = Layer_Dense(10, 6)
+layer2.forward(layer1.output)
+print(layer2.output)
+'''
+np.random.seed(0)
+
+def create_data(points, classes):
+    x = np.zeros((points*classes, 2))
+    y = np.zeros(points*classes, dtype='uint8')
+    for class_number in range(classes):
+        ix = range(points*class_number, points*(class_number+1))
+        r = np.linspace(0.0, 1, points) #radius
+        t = np.linspace(class_number*4, (class_number+1)*4, points) + np.random.randn(points)*0.2
+        x[ix] = np.c_[r*np.sin(t*2.5), r*np.cos(t*2.5)]
+        y[ix] = class_number
+    return x, y
+ 
+print("here")
+x, y = create_data(100, 3)
+
+plt.scatter(x[:,0], x[:,1])
+plt.show()
+
+plt.scatter(x[:,0], x[:,1], c=y, cmap="brg")
+plt.show()
+
+
+
+
