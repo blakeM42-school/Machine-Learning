86301a7229365d5b1441079e171f7c93797b4bce
blakeM42-school <141346707+blakeM42-school@users.noreply.github.com>
2024-03-20 19:54:47 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import nnfs
+from nnfs.datasets import spiral_data
 
 nnfs.init()
-'''
+
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
@@ -16,35 +17,28 @@ class Activation_ReLU:
     def forward(self, inputs):
         self.output = np.maximum(0, inputs)
 
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
 
-layer1 = Layer_Dense(40, 10)
-layer1.forward()
-print(layer1.output)
-layer2 = Layer_Dense(10, 6)
-layer2.forward(layer1.output)
-print(layer2.output)
-'''
-np.random.seed(0)
 
-def create_data(points, classes):
-    x = np.zeros((points*classes, 2))
-    y = np.zeros(points*classes, dtype='uint8')
-    for class_number in range(classes):
-        ix = range(points*class_number, points*(class_number+1))
-        r = np.linspace(0.0, 1, points) #radius
-        t = np.linspace(class_number*4, (class_number+1)*4, points) + np.random.randn(points)*0.2
-        x[ix] = np.c_[r*np.sin(t*2.5), r*np.cos(t*2.5)]
-        y[ix] = class_number
-    return x, y
- 
-print("here")
-x, y = create_data(100, 3)
+X, y = spiral_data(samples=100, classes=3)
 
-plt.scatter(x[:,0], x[:,1])
-plt.show()
+dense1 = Layer_Dense(2,3)
+activation1 = Activation_ReLU()
 
-plt.scatter(x[:,0], x[:,1], c=y, cmap="brg")
-plt.show()
+dense2 = Layer_Dense(3,3)
+activation2 = Activation_Softmax()
+
+dense1.forward(X)
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+print(activation2.output)
 
 
 
